68f75fd7ad97e3c6a8dddea722a4eb85b6ff824f
temeculous <zacerious@gmail.com>
2025-01-24 16:29:41 -0500

    Finalized initial crud file

--- src/crud.py
+++ src/crud.py
@@ -60,3 +60,60 @@ def create_user(db: Session, user: schemas.UserCreate) -> models.User:
         db.rollback()
         raise ValueError("Username or Email already exists")
     
+def get_user_by_username(db: Session, username: str) -> typing.Optional[models.User]:
+    '''
+    Retrieve user by username
+
+    Args: db session, username to search for
+
+    Returns: user model if found, else returns nothing
+    '''
+    return db.query(models.User).filter(models.User.username == username).first()
+
+def get_user_by_email(db: Session, email: str) -> typing.Optional[models.User]:
+    '''
+    Retrieve user by email
+
+    Args: db session, email to search for
+
+    Returns: user model if found, else returns nothing
+    '''
+    return db.query(models.User).filter(models.User.email == email).first()
+
+def create_service_request(
+    db: Session, 
+    service_request: schemas.ServiceRequestCreate
+) -> models.ServiceRequest:
+    '''
+    Create new service request
+
+    Args: db session, service creation schema
+
+    Returns: created service request model
+    '''
+    db_service_request = models.ServiceRequest(
+        title=service_request.title,
+        description=service_request.description,
+        requester_id=service_request.requester_id
+    )
+    
+    db.add(db_service_request)
+    db.commit()
+    db.refresh(db_service_request)
+    
+    return db_service_request
+
+
+def get_service_requests(
+    db: Session, 
+    skip: int = 0, 
+    limit: int = 100
+) -> typing.List[models.ServiceRequest]:
+    '''
+    Retrieve service requests with pagination
+
+    Args: db session, number of records to skip, max num of records to return
+
+    Returns: list of service request models
+    '''
+    return db.query(models.ServiceRequest).offset(skip).limit(limit).all()
